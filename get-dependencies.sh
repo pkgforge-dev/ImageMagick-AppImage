d34bcc57643fb3641505f19ea1f37b75eab64d62
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-01-31 07:17:00 -0400

    get app

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,7 +6,7 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-# pacman -Syu --noconfirm PACKAGESHERE
+pacman -Syu --noconfirm imagemagick
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
