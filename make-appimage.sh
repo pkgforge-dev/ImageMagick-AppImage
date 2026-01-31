0e0ac1f22246f48a01dc48af3672031f67f78173
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-01-31 07:23:06 -0400

    package app

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,16 +3,28 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q PACKAGENAME | awk '{print $2; exit}') # example command to get version of application here
+VERSION=$(pacman -Q imagemagick | awk '{print $2; exit}') # example command to get version of application here
 export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=PATH_OR_URL_TO_ICON
-export DESKTOP=PATH_OR_URL_TO_DESKTOP_ENTRY
+export ICON=https://github.com/ImageMagick/ImageMagick/blob/main/app-image/icon.png?raw=true
+export DESKTOP=https://raw.githubusercontent.com/ImageMagick/ImageMagick/refs/heads/main/app-image/imagemagick.desktop
 
 # Deploy dependencies
-quick-sharun /PATH/TO/BINARY_AND_LIBRARIES_HERE
+quick-sharun \
+	/usr/bin/animate   \
+	/usr/bin/compare   \
+	/usr/bin/composite \
+	/usr/bin/conjure   \
+	/usr/bin/convert   \
+	/usr/bin/display   \
+	/usr/bin/identify  \
+	/usr/bin/import    \
+	/usr/bin/magick*   \
+	/usr/bin/mogrify   \
+	/usr/bin/montage   \
+	/usr/bin/stream
 
 # Additional changes can be done in between here
 
